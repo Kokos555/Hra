be3a4ad8bb053ec9153bc1bb0e8b0a10893114fb
novotnyv.03 <novotnyv.03@A222-14>
2023-03-23 15:02:58 +0100

    Přidejte soubory projektu.

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,68 @@
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Name_POSITIVE()
+        {
+            string name = "Vojtasdhjuidashfuidf";
+            Assert.IsTrue(name.Length > 10);
+        }
+
+        [TestMethod]
+        public void Name_NEGATIVE()
+        {
+            string name = "Vojta";
+            Assert.IsTrue(name.Length > 10);
+        }
+        [TestMethod]
+        public void Name_IsNull()
+        {
+            string name = "Vojta";
+            Assert.IsNotNull(name);
+        }
+        [TestMethod]
+        public void Name_IsNotNull()
+        {
+            string name = null;
+            Assert.IsNotNull(name);
+        }
+        [TestMethod]
+        public void Level_POSITIVE()
+        {
+            int level = 1;
+            Assert.AreEqual(1, level);
+        }
+        [TestMethod]
+        public void Level_NEGATIVE()
+        {
+            int level = 1;
+            Assert.AreEqual(2, level);
+        }
+        [TestMethod]
+        public void X_POSITIVE()
+        {
+            int x = 0;
+            Assert.AreEqual(0, x);
+        }
+        [TestMethod]
+        public void X_NEGATIVE()
+        {
+            int x = 0;
+            Assert.AreEqual(80, x);
+        }
+        [TestMethod]
+        public void Y_POSITIVE()
+        {
+            int y = 0;
+            Assert.AreEqual(0, y);
+        }
+        [TestMethod]
+        public void Y_NEGATIVE()
+        {
+            int y = 0;
+            Assert.AreEqual(80, y);
+        }
+    }
+}
